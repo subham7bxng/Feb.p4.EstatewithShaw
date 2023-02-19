0593ec0d746694c9e4178fb3d0858716b6af8cc3
subham7bxng <subhamshawufc@gmail.com>
2023-02-19 14:03:32 +0530

    push

---
+++ mian.js
@@ -0,0 +1,8 @@
+
+// var imgy = document.querySelectorAll("cities").
+
+function city(){
+    var nameU = prompt("your Name :");
+    var num = prompt("Number :");
+    alert("Thanku for contracting "+nameU+ ", our team will contact you!");
+}
